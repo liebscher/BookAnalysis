ce409c9cc1a67b63145871bc3f1e2660a1c497c2
Alex Liebscher <alexliebscher0@gmail.com>
2017-12-21 17:58:16 -0800

    Plot support, single book testing

--- regression.py
+++ regression.py
@@ -1,75 +1,70 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.pyplot import xticks, grid
 from sklearn import linear_model
-from sklearn.utils import shuffle
+from sklearn.model_selection._split import train_test_split
+
+# Labeled columns
+headers = ('five_star', 'european', 'north_american', 'asian', 'other', 'author_gender', 'avg_rating', 'pages',
+           'org_publication_year', 'duration', 'favorite', 'fiction', 'nonfiction', 'contemporary',
+           'philosphy', 'politics', 'religion', 'science_tech_math', 'short_story', 'memoir', 'war_story', 'historical')
 
 # Load machine ready data
 data = pd.read_csv('machine_library.csv')
-
-# Total iterations of fitting
-iterations = 250
-
-# Total increments of j
-increments = 25
 
 # Define our custom error rate function, returns a result [0-1]. 1: perfect fit, 0: opposite fit
 def error_rate(h, y):
     l = len(h)
     error = 0
     for i in range(l):
-        error += (abs(y[i] - h[i])/4)
+        error += (abs(y[i] - h[i]) / 2)
 
-    return 1-error/l
+    return 1 - error / l
+
 
 # Define our modeling function. Parameter j sets the training/testing data ratio.
 # Returns (hypothesis target data, true target data, error rate) tuple
-def fit(j):
-    # Determine training set portion
-    split = int(data.shape[0] * j)
-
+def fit(j, testing=None, coef=False):
     # Set training data and target data
-    X = np.array(data.loc[:, np.delete(data.columns.values, 0)])
-    Y = np.array(data.loc[:, ['my_rating']]).ravel()
+    X = np.array(data.loc[1:, np.delete(data.columns.values, 0)])
+    Y = np.array(data.loc[1:, ['five_star']]).ravel()
 
-    # Instantiate multiclass classification logistic regression model
+    # Assign the training/testing sets
+    X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=1 - j)
+
+    # Assign the testing set to the new vars if necessary
+    if testing is None:
+        testing = [X_test, Y_test]
+
+    # Instantiate multi-class classification logistic regression model
     logreg = linear_model.LogisticRegression(C=1e5)
 
     # Fit model
-    logreg.fit(X[0:split], Y[0:split])
+    logreg.fit(X_train, Y_train)
 
     # Set hypothesis and true target data
-    h = logreg.predict(X[split:])
-    y = Y[split:]
+    h = logreg.predict(testing[0])
+    y = testing[1]
 
-    return (h,y,error_rate(h,y))
+    # Return the coefficient matrix if necessary
+    if coef:
+        return (h, y, error_rate(h, y), logreg.coef_)
+
+    return (h, y, error_rate(h, y))
 
 
-# Range of increments to use. Must cut of low end and high end to ensure valid training/testing portions
-rng = (int(increments*0.2), int(increments*0.9))
+# Use the first line of the data csv for single book testing
+x = np.array(data.loc[0, np.delete(data.columns.values, 0)])
 
-# Define our results DataFrame, organized by j value, which dictates split ratio
-# Designed to maximize training data for this small data set
-results = pd.DataFrame(columns=[str(a/increments) for a in range(rng[0], rng[1])], index=[0])
+# Fit and predict with the new model
+f = fit(.76, testing=[[x],[1]], coef=True)
+#print((f[0],f[1],f[2]))
 
-for _ in range(iterations):
-    # Randomly shuffle rows
-    data = shuffle(data)
-
-    for j in range(rng[0], rng[1]):
-        # Append error rate, of hypothesis and true target data, to j column in results DF
-        results = results.append({str(j/increments): fit(j/increments)[2]}, ignore_index=True)
-
-# Sample
-f = fit(.78)
-print(f[0])
-print(f[1])
-print(f[2])
-
-# [5 3 2 4 4 4 4 5 2 5 5]
-# [3 3 4 4 3 4 3 5 2 5 5]
-# 0.863636363636
-
-results.plot.box(title='Effect of training data ratio on Error Rate')
+# Plot coefficient matrix
+weights = pd.DataFrame(np.array(f[3]).transpose(), columns=['5 star'])
+weights.plot.bar()
+grid(True, alpha=0.4)
+xticks(range(len(headers[1:])), headers[1:], rotation=45)
 
 plt.show()
